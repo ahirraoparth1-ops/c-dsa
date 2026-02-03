47ab2ba2b2213b1f4fbfeeee29153a503b2922f7
arjunAhirrao90@gmail.com <ahirraoparth1@gmail.com>
2026-02-03 11:43:53 +0530

    print sum of n using parametrised way

---
+++ ps6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+ void fun (int i,int sum){
+    
+    if(i<1){
+
+       cout<<sum<<endl;
+        return;    
+        
+    }
+    fun(i-1, sum+i);    
+    
+
+    
+ }
+ 
+ int main(){
+    int n=3;
+    fun(n,0);
+    
+}
